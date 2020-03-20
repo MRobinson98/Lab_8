62686b42f25469640ad07f3c31716b8d7e47d11e
Matthew Robinson <robinson98.m@gmail.com>
2020-03-20 14:57:31 -0400

    test

---
+++ index.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+
+<HTML lang="en">
+
+<HEAD>
+	<META charset="utf-8">
+	<META name="author" content="Matthew Robinson">
+
+	<TITLE>Template</TITLE>
+
+	<link href="Stylesheet.css" rel="stylesheet" type="text/css">
+
+</HEAD>
+
+<BODY>
+	<div class="gridPage">
+		<!---Header--->
+		<Header>
+<div class="login">
+				<input type="text" placeholder="Enter Email" name="email" required>
+				<input type="text" placeholder="Enter Password" name="pass" required>
+				<button type="submit">Login</button>
+			</div>
+
+			<div class="loginTest">
+				<input type="button" onclick="location.href='GuideProfile.html';" value="Guide Profile" />
+				<input type="button" onclick="location.href='CustomerProfile.html';" value="Customer Profile" />
+				<input type="button" onclick="location.href='EditPricing.html';" value="Admin Profile" />
+			 </div>
+
+			<p><a href="index.html"> Mt. Muriel Ski Guides </a></p>
+			<div class="smallHeader">
+				<p> Professional and Fun Adventures! </p>
+			</div>
+
+
+		</Header>
+
+		<!---End Header--->
+
+		<!---Nav-->
+		<Nav>
+			<H2> <span class="hyperlinks"><a href="Destinations.html">Destinations</a></span> <span class="hyperlinks"><a href="Pricing.html">Pricing</a></span> <span class="hyperlinks"><a href="Safety.html">Safety</a></span> <span class="hyperlinks"><a
+						href="Register.html">Register </a></span> <span class="hyperlinks"><a href="Reviews.html">Reviews</a></span> <span class="hyperlinks"><a href="aboutMe.html"> About </a></span><span class="hyperlinks"><a href="Register.html">
+						Register
+					</a></span></H2>
+		</Nav>
+		<!---End Nav-->
+
+		<!---Main Content--->
+		<div class="content">
+
+		</div>
+
+
+		<!---Footer--->
+		<FOOTER>
+			<div class="imgRow">
+
+				<a href="https://www.acmg.ca/">
+					<img src="resources/ACMG.jpg" alt="ACMG">
+				</a>
+
+
+
+				<a href="https://www.avalanche.ca/">
+					<img src="resources/AvyCanada.jpg" alt="AvyCanada">
+				</a>
+
+
+				<a href="https://https://www.blackdiamondequipment.com/">
+					<img src="resources/BlackDiamond.jpg" alt="Black Diamond Equipment">
+				</a>
+
+
+
+				<img src="resources/Facebook.png" alt="Facebook">
+
+
+				<img src="resources/Instagram.png" alt="Instagram">
+
+				<img src="resources/Youtube.png" alt="Youtube">
+			</div>
+
+			<H5>COPYRIGHT 2020 MT. Muriel Ski Guides
+				<div>Icons made by <a href="https://www.flaticon.com/authors/pixel-perfect" title="Pixel perfect">Pixel perfect</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+			</H5>
+
+		</FOOTER>
+		<!---End Footer--->
+	</div>
+</BODY>
+
+</HTML>
